2cb16665d47d95a0f442b3a119d594b0ead11a4f
Edily Mora <edilyrosa@gmail.com>
2025-02-24 19:36:42 -0400

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,177 @@
+//esta informacion es para el funcionamiento de emailjs, y quedaria expuesta en GitHub.
+const PUBLIC_KEY = "4Y77DFjgko8rvBzwf";
+const ID_TEMPLATE = "template_a4yygd8";
+const ID_SERVICE = "service_iuya47f";
+
+
+console.log('escuchando el DOMContentLoaded');
+
+document.addEventListener('DOMContentLoaded', () => {
+    emailjs.init(PUBLIC_KEY);
+    cargarProyectos();
+    configurarFormulario();
+    agregarValidacionTiempoReal();
+});
+
+
+//! Para mantener el JSON como un archivo separado y consumirlo dinámicamente, necesitarías usar 
+//! JavaScript. fetch() es una opción común y moderna para esto
+async function cargarProyectos() { //^Cargar asincronamente la data mostrada en div #proyectos-container
+    // el loader es un elemento que se esta mostrando de una.
+    const container = document.getElementById('proyectos-container');
+    const loader = document.getElementById("loader");
+    try {
+        const response = await fetch('./data.json');
+        const data = await response.json();
+        // Remueve el loader
+        loader.remove();
+        data.forEach(proyecto => {
+            const card = crearProyectoCard(proyecto);
+            container.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Error al cargar los proyectos:', error);
+    }
+}
+
+function crearProyectoCard(proyecto) { //^Machote de la card con la informacion de un proyecto
+    const card = document.createElement('div');
+    card.className = 'shadow-lg rounded-lg overflow-hidden transform transition-all duration-300 hover:-translate-y-2 hover:shadow-xl w-full max-w-sm';
+
+    card.innerHTML = `
+    <div class="proyecto-info p-4 bg-white w-full min-h-[45vh]">
+    <img src="${proyecto.imagen}" alt="${proyecto.titulo}" class="w-full object-fit">
+            <h3 class="text-lg font-semibold mb-2">${proyecto.titulo}</h3>
+            <p class="text-sm text-gray-600 flex-grow">${proyecto.descripcion}</p>
+            <a href="${proyecto.url}" target="_blank" class="text-blue-500 hover:underline mt-2 block"><i>${proyecto.url}</i></a>
+        </div>
+    `;
+
+    return card;
+}
+
+
+function agregarValidacionTiempoReal() { //^validamos los input y textarea en tiempo real
+    const inputs = document.querySelectorAll('#contact-form input, #contact-form textarea');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', () => validarCampo(input));
+        input.addEventListener('blur', () => validarCampo(input));
+    });
+}
+
+function validarCampo(input) {//^patrones de validaciones por entrada (que tenga blur o se le este typiando) y su mensajeria
+    const nombreRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    let isValid = true;
+    let mensaje = '';
+
+    if (input.id === 'name' && !nombreRegex.test(input.value)) {
+        isValid = false;
+        mensaje = 'Estas escribiendo un Nombre inválido';
+    } else if (input.id === 'email' && !emailRegex.test(input.value)) {
+        isValid = false;
+        mensaje = 'Estas escribiendo un Email inválido';
+    } else if (input.id === 'message' && input.value.length > 1000) {
+        isValid = false;
+        mensaje = 'El mensaje no debe exceder los 1000 caracteres.';
+    }
+
+    if (!isValid) {
+        mostrarError(input.id, mensaje);
+    } else {
+        ocultarError(input.id);
+    }
+}
+
+function mostrarError(campo, mensaje) {
+    const input = document.getElementById(campo);
+    const errorSpan = document.getElementById(`${campo}-error`);
+    input.classList.add('error');
+    errorSpan.textContent = mensaje;
+    errorSpan.style.display = 'block';
+}
+
+function ocultarError(campo) {
+    const input = document.getElementById(campo);
+    const errorSpan = document.getElementById(`${campo}-error`);
+    input.classList.remove('error');
+    errorSpan.style.display = 'none';
+}
+
+function validarFormulario() {//^Booleano: Patrones, validamos y mensajeria de las entradas al evento "submit" NO realtime.
+    const nombre = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('message').value;
+
+    const nombreRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s-]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    let isValid = true;
+
+    if (!nombreRegex.test(nombre)) {
+        mostrarError('name', 'Nombre inválido');
+        console.log('Nombre inválido');
+        
+        isValid = false;
+    } else {
+        ocultarError('name');
+    }
+
+    if (!emailRegex.test(email)) {
+        mostrarError('email', 'Email inválido');
+        isValid = false;
+    } else {
+        ocultarError('email');
+    }
+
+    if (mensaje.length > 1000) {
+        mostrarError('message', 'El mensaje no debe exceder los 1000 caracteres.');
+        isValid = false;
+    } else {
+        ocultarError('message');
+    }
+
+    return isValid;
+}
+
+function configurarFormulario() {//^submit del form mas validaciones del subtmit (no realtime)
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        if (validarFormulario()) {
+            try {
+                await enviarEmail();
+                alert('Mensaje enviado con éxito!');
+                form.reset();
+            } catch (error) {
+                console.error('Error al enviar el email:', error);
+                alert('Hubo un error al enviar el mensaje. Por favor, intenta de nuevo.');
+            }
+        }
+    });
+}
+async function enviarEmail() {
+    const templateParams = {
+        from_name: document.getElementById('name').value,
+        from_email: document.getElementById('email').value,
+        message: `Nombre: ${document.getElementById('name').value}\nEmail: ${document.getElementById('email').value}\nMensaje: ${document.getElementById('message').value}`
+    };
+
+    try {
+        const response = await emailjs.send(ID_SERVICE, ID_TEMPLATE, templateParams);
+        if (response.status === 200) {
+            console.log('Email enviado con éxito:', response);
+            return true;
+        } else {
+            throw new Error('Error al enviar el email');
+        }
+    } catch (error) {
+        console.error('Error al enviar el email:', error);
+        throw error;
+    }
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// // Este es el archivo de configuración principal de Tailwind CSS, 
+// // donde defines personalizaciones como colores, fuentes y temas
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   // Indica a Tailwind qué archivos debe analizar en busca de clases de Tailwind.
+//   content: ["./src/**/*.{html,js}"],
+
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         poppins: ["Poppins", "sans-serif"], // Define 'poppins' como una fuente personalizada
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
